b24bdff0a4f42ae54d84913f560877ec4c30ef95
zhoufei <15003707121@163.com>
2018-06-01 17:21:02 +0800

    账户中心

---
+++ js/custom/home.js
@@ -0,0 +1,6 @@
+$(".informationManagement li").on("touchstart",function(){
+	$(this).css("background","#eee");
+});
+$(".informationManagement li").on("touchend",function(){
+	$(this).css("background","#fff");
+});
