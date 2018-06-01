12a3945c79c82ac8a9f5064076c45a558e3d9c49
zhoufei <15003707121@163.com>
2018-06-01 19:58:57 +0800

    文章页面

--- js/custom/discover.js
+++ js/custom/discover.js
@@ -7,18 +7,18 @@ $(function(){
 			for (var i = 0;i < data.length;i++) {
 				$(".storyList").append('<li><article></article><div class="storySummary"><div class="summaryLeft"><h1>'+data[i].noteTitle+'</h1><h2>'+data[i].noteSummary+'</h2></div><span class="publicationTime" >'+data[i].noteTime+'</span></div></li>')
 			}
+			
+			/* 点击效果 */
+			$(".storySummary").on("touchstart",function(){
+				$(this).css("background","#eee");
+				window.location.href = "html/noteContent.html"
+			})
+			$(".storySummary").on("touchend",function(){
+				$(this).css("background","#fff");
+			})
 		},
 		error:function(){
 			alert("网络访问错误");
 		}
 	});
-	
-	
-	
-	$(".storySummary").on("touchstart",function(){
-		$(this).css("background","#eee");
-	})
-	$(".storySummary").on("touchend",function(){
-		$(this).css("background","#fff");
-	})
 })
