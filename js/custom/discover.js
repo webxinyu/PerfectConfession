57a9659eaf6323ebd9cf982487acf2f8dbbb9d67
zhoufei <15003707121@163.com>
2018-06-01 18:47:32 +0800

    模拟数据

--- js/custom/discover.js
+++ js/custom/discover.js
@@ -1,4 +1,20 @@
 $(function(){
+	$.ajax({
+		type:"get",
+		url:"data/discover.json",
+		async:true,
+		success:function(data){
+			for (var i = 0;i < data.length;i++) {
+				$(".storyList").append('<li><article></article><div class="storySummary"><div class="summaryLeft"><h1>'+data[i].noteTitle+'</h1><h2>'+data[i].noteSummary+'</h2></div><span class="publicationTime" >'+data[i].noteTime+'</span></div></li>')
+			}
+		},
+		error:function(){
+			alert("网络访问错误");
+		}
+	});
+	
+	
+	
 	$(".storySummary").on("touchstart",function(){
 		$(this).css("background","#eee");
 	})
