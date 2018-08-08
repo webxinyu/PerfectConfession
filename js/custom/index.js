a0595875b915a9fe69036f96722caedd2a6a9c96
zhoufei <15003707121@163.com>
2018-08-08 16:33:28 +0800

    逻辑优化

--- js/custom/index.js
+++ js/custom/index.js
@@ -15,3 +15,50 @@ $("#openBagsListBtn").on("touchend",function(){
 $("#openBagsListBtn").on("touchstart",function(){
 	window.location.href = "html/shoppingList.html";
 });
+/* 点击添加按钮 */
+$(".addShoppingCart").on("click",function(){
+	var imgSrc = $(this).parent().parent().find(".shopImg").find("img")[0].src;		//当前添加商品的图片路径
+	$(".footerPopUp").css("display","block");
+	$("#shoppingImg")[0].src = imgSrc;
+	
+});
+/* 关闭添加购物车弹窗 */
+$(".closeShopInf").on("click",function(){
+	$(".footerPopUp").css("display","none");
+})
+/* 购买数量增加 */
+var startNum = 0;
+$("#shopJia").on("click",function(){
+	startNum = parseInt($(this).parent().find(".shopSum").text())+1;
+	if (startNum >= parseInt($("#surplusNum").text())) {
+		startNum = parseInt($("#surplusNum").text());
+		$(this).parent().find(".shopSum").text(startNum);
+		buttonColorNot("shopJia");
+	} else{
+		buttonColorCan("shopJia");
+		buttonColorCan("shopJian");
+		$(this).parent().find(".shopSum").text(startNum);
+	}
+	
+})
+$("#shopJian").on("click",function(){
+	startNum = parseInt($(this).parent().find(".shopSum").text())-1;
+	if (startNum <= 1) {
+		startNum = 1;
+		$(this).parent().find(".shopSum").text(startNum);
+		buttonColorNot("shopJian");
+	} else{
+		buttonColorCan("shopJian");
+		buttonColorCan("shopJia");
+		$(this).parent().find(".shopSum").text(startNum);
+	}
+	
+})
+function buttonColorNot(btnDom){		//按钮颜色变灰
+	$("#"+btnDom).css("borderColor","#ccc");
+	$("#"+btnDom).find(".iconfont").css("color","#ccc");
+}
+function buttonColorCan(btnDom){		//按钮颜色变量
+	$("#"+btnDom).css("border","1px #999 solid");
+	$("#"+btnDom).find(".iconfont").css("color","#999");
+}
