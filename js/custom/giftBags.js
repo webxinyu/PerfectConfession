91536160572eea9d3cfb840b9eb0edb7c7131cca
zhoufei <15003707121@163.com>
2018-08-16 15:19:16 +0800

    页面增加

--- js/custom/giftBags.js
+++ js/custom/giftBags.js
@@ -17,6 +17,25 @@ $(function(){
 		window.location.href = 'html/orderPage.html';
 	});
 	$(".wasteClassification").on("click","li",function(){
-		window.location.href = 'html/subpage/reclamationDetails.html';
+		console.log($(this).find("h2").text());
+		if ($(this).find("h2").text() == "废纸") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=1';
+		}
+		if ($(this).find("h2").text() == "饮料瓶") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=2';
+		}
+		if ($(this).find("h2").text() == "金属") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=3';
+		}
+		if ($(this).find("h2").text() == "塑料") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=4';
+		}
+		if ($(this).find("h2").text() == "纺织物") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=5';
+		}
+		if ($(this).find("h2").text() == "玻璃") {
+			window.location.href = 'html/subpage/reclamationDetails.html?classify=6';
+		}
+		
 	})
 })
