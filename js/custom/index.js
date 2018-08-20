814ee9c717d13663d8dc6293375b33e301c7e051
zhoufei <15003707121@163.com>
2018-08-20 18:28:48 +0800

    优化购物车

--- js/custom/index.js
+++ js/custom/index.js
@@ -1,3 +1,7 @@
+/* 页面进来创建购物车数组的本地存储 */
+var storageShopCart = [];
+localStorage.storageShopCart = JSON.stringify(storageShopCart);
+
 var mySwiper = new Swiper('.swiper-container', {
 	autoplay: true,//可选选项，自动滑动
 	loop : true,
@@ -16,12 +20,11 @@ $("#openBagsListBtn").on("touchstart",function(){
 	window.location.href = "html/shoppingList.html";
 });
 /* 点击添加按钮 */
-$(".addShoppingCart").on("click",function(event){	
+$(".addShoppingCart").on("click",function(event){
 	var imgSrc = $(this).parent().parent().find(".shopImg").find("img")[0].src;		//当前添加商品的图片路径
 	$(".footerPopUp").css("display","block");
 	$("#shoppingImg")[0].src = imgSrc;
-	event.stopPropagation();
-	
+	event.stopPropagation();	
 });
 /* 关闭添加购物车弹窗 */
 $(".closeShopInf").on("click",function(){
@@ -86,4 +89,57 @@ $("#openIntegralPurchase").on("click",function(){		//积分购
 /* 进入购物车 */
 $(".lookShoppingCart").on("click",function(){
 	window.location.href = 'html/subpage/shopCartList.html'
+});
+/* 加入购物车 */
+$("#indexAddCart").on("click",function(){
+	var flag = false;		//判断本地存储购物车是够有选中商品的判断的开关
+	var thisSopIndex = 0;
+	//获取存储在本地的购物车列表
+	storageShopCart = JSON.parse(localStorage.storageShopCart);
+	//获取商品id*** 注释：这里用图片id模仿 ***
+	var shopId = getImgName();
+	//获取商品的价格
+	var shopPrice = $("#nowShopPrice").text();
+	//获取商品的数量
+	var shopSum = $(".shopSum").text();
+	//当前商品信息
+	var shopArr = {"shopId":shopId,"shopPrice":shopPrice,"shopSum":shopSum}
+	//判断本地存储的购物车列表中是否有当前选择的商品
+	for (var i = 0; i < storageShopCart.length; i++) {
+		if (storageShopCart[i].shopId == shopId) {
+			flag = true;
+			thisSopIndex = i;
+		} else{
+			flag = false;
+		}
+	}
+	if (flag) {		//有当前商品
+		storageShopCart.splice(thisSopIndex,1,shopArr);
+	} else{			//没有当前商品
+		storageShopCart.push(shopArr);
+	}
+	localStorage.storageShopCart = JSON.stringify(storageShopCart);
 })
+
+
+
+
+
+
+/* 项目用到的所有的公共方法 */
+function getImgName() {
+	//先得到图片
+	var img=document.getElementById("shoppingImg");
+	//得到图片路径
+	var path=img.src;
+	//从路径中截取图片名[包括后缀名]
+	var filename;
+	if(path.indexOf("/")>0){		//如果包含有"/"号 从最后一个"/"号+1的位置开始截取字符串
+	    filename=path.substring(path.lastIndexOf("/")+1,path.length);
+	}
+	else{
+	    filename=path;		
+	}
+	return filename;	
+}
+
